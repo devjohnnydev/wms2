3157e860794dcd3a0a39af60c3377e2344130596
cDorth <carloseduardodorth123@gmail.com>
2026-03-06 16:32:06 -0300

    iniciação de inicio

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export PATH="/opt/venv/bin:$PATH"
 export PYTHONPATH=$PYTHONPATH:$(pwd)/SGA-Backend
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu
 
 # Inicia o backend (que serve o frontend)
 cd SGA-Backend
